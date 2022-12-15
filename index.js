f8fd50b19c4cd30d64e5823482b64f67e393bb0e
Soyy <81018230+haophuc-tonthat@users.noreply.github.com>
2022-12-15 23:58:57 +0700

    Remove File JS

--- index.js
+++
@@ -1,9 +0,0 @@
-// Variables
-const btnSubmit = document.querySelector("#btnSubmit");
-const input = document.querySelector("#wish");
-
-btnSubmit.addEventListener("click", (ev) => {
-  ev.preventDefault();
-  console.log(document.getElementById("wish").value);
-  var data = document.getElementById("wish").value;
-});
